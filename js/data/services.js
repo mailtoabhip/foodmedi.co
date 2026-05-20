d6b43b5e7ef4f879ae0f23c072e98d50e8b74f20
Abhip <mailtoabhip@gmail.com>
2026-05-21 00:47:04 +0530

    3-column services grid; hide 3-Month Package

--- js/data/services.js
+++ js/data/services.js
@@ -193,5 +193,5 @@ export const SERVICES = {
 };
 
 /* To show hidden services, move keys back into VISIBLE_SERVICES */
-export const VISIBLE_SERVICES = ['initial', 'followup', 'package', 'group', 'corporate', 'oncology'];
+export const VISIBLE_SERVICES = ['initial', 'followup', 'group', 'corporate', 'oncology'];
 export const SERVICE_ORDER    = VISIBLE_SERVICES;
